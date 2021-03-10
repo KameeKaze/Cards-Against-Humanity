79b91582b0f72c9eb9cd676968986d86982da20c
karak1974 <wolfyinboc@gmail.com>
2021-03-10 20:49:20 +0100

    Embed update & Dockerfile added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Cards-Against-Humanity
+
+To build it
+`sudo docker build -t cah .`
+
+To run it
+`sudo docker run cah`
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from random import choices
 from random import choice
 
 bot = commands.Bot(command_prefix=">")
+bot.remove_command('help') # own help menu near to the end
 
 @bot.event
 async def on_ready():
@@ -27,7 +28,15 @@ class User:
         for x in self.cards:
             whites.remove(x)
             
-
+#pinging a bot
+@bot.command(aliases = ["p"])
+async def ping(ctx):
+    ping_ = bot.latency
+    ping =  round(ping_ * 1000)
+    embed = discord.Embed(title="Ping", value=f"{ping}", color=discord.Color.red())
+    embed.add_field(name=f"{ping}", value="ms")
+    embed.set_footer(text="CAH", icon_url="https://wolfyfiles.000webhostapp.com/home/discord/cah.jpg")
+    await ctx.send(embed=embed)
 
 # join to game
 @bot.command()
@@ -36,19 +45,50 @@ async def join(ctx):
     userlist.append(User(choices(whites,k=card_numer),author.name,author))
 
     cards = userlist[-1].cards
-    await author.send("Here are your cards:\n\n"+"\n".join(cards))
+    #await author.send("Here are your cards:\n\n"+"\n".join(cards))
+
+    cardlist = "\n".join(cards) 
+    embed = discord.Embed(title="Üdvözöllek a játékban", color=discord.Color.blue())
+    embed.add_field(name = 'Kártyáid:', value=cardlist, inline = True)
+    embed.set_footer(text="CAH", icon_url="https://wolfyfiles.000webhostapp.com/home/discord/cah.jpg")
+    await author.send(embed=embed)
+
+#list the cards of the user
 
 
 
 #list the cards of the user
 @bot.command()
 async def cards(ctx):
-    await userlist[0].user.send("\n".join(userlist[0].cards))
+    #await userlist[0].user.send("\n".join(userlist[0].cards))
+
+    cardlist = "\n".join(userlist[0].cards)
+    embed = discord.Embed(title="Cards Against Humanity", color=discord.Color.green())
+    embed.add_field(name = 'Kártyáid:', value=cardlist, inline = True)
+    embed.set_footer(text="CAH", icon_url="https://wolfyfiles.000webhostapp.com/home/discord/cah.jpg")
+    await ctx.author.send(embed=embed)
 
 # list all users in the game
 @bot.command()
 async def users(ctx):
-    await ctx.author.send("\n".join(x.name for x in userlist))
+    #await ctx.author.send("\n".join(x.name for x in userlist))
+
+    userl="\n".join(x.name for x in userlist)
+    embed = discord.Embed(title="Cards Against Humanity", color=discord.Color.orange())
+    embed.add_field(name = 'jelenlegi játékosok:', value=userl, inline = True)
+    embed.set_footer(text="CAH", icon_url="https://wolfyfiles.000webhostapp.com/home/discord/cah.jpg")
+    await ctx.author.send(embed=embed)
+
+@bot.command(aliases = ["h"])
+async def help(ctx):
+    embed = discord.Embed(title="Segítség", color=discord.Color.green())
+    embed.add_field(name=">help", value="Help menü")
+    embed.add_field(name=">join", value="Belépés a játékbai")
+    embed.add_field(name=">cards", value="Jelenlegi kártyáid")
+    embed.add_field(name=">users", value="Játékban levő játékosok")
+    embed.add_field(name=">ping", value="Ping ellenörzés")
+    embed.set_footer(text="CAH", icon_url="https://wolfyfiles.000webhostapp.com/home/discord/cah.jpg") 
+    await ctx.author.send(embed=embed)
 
 
 #start the game
@@ -62,9 +102,4 @@ async def start(ctx):
         for n,card in enumerate(user.cards):
             await user.user.send(f"[{n}] {card}")
 
-
-
-
-
-
-bot.run("")
+bot.run("ODE5Mjc2NTUxODI4NjY4NDc2.YEkQvw.R4N5BceHHkG0DiKRdA5Dg_adtiE")

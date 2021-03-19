ddfb94436ace960f1d88978040aaff3a58de029d
kamee <k4m33k4z3@protonmail.com>
2021-03-19 16:23:33 +0000

    almost done

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ reactions = ["1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣","7️�
 icon = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Cards_Against_Humanity_logo.png/220px-Cards_Against_Humanity_logo.png"
 
 class User:
-    score = 0
     def __init__(self,cards,name,user):
         self.cards = cards
         self.name = name
         self.user = user
+        self.score = 0
         for x in self.cards:
             whites.remove(x)
             
@@ -45,24 +45,29 @@ async def ping(ctx):
 #join to the game
 @bot.command(aliases = ["j"])
 async def join(ctx):
-    author=ctx.author
+    if len(userlist) >= 10:
+        embed = discord.Embed(title="Error: too many users", color=discord.Color.red())
+        await ctx.author.send(embed=embed)
+    else:
+        
+        author=ctx.author
 
-    # check if user already joined
-    if all([x.user != author for x in userlist]):
-        # create new user
-        userlist.append(User(choices(whites,k=card_numer),author.name,author))
+        # check if user already joined
+        if all([x.user != author for x in userlist]):
+            # create new user
+            userlist.append(User(choices(whites,k=card_numer),author.name,author))
 
-        # get the new user's cards
-        cards = "\n".join(userlist[-1].cards)
+            # get the new user's cards
+            cards = "\n".join(userlist[-1].cards)
 
-        # send join message and cards
-        embed = discord.Embed(title="Wellcome to the game", color=discord.Color.blurple())
-        embed.add_field(name = 'Your cards:', value=cards, inline=True)
-        embed.set_footer(text="CAH", icon_url=icon)
-        await author.send(embed=embed)
+            # send join message and cards
+            embed = discord.Embed(title="Wellcome to the game", color=discord.Color.blurple())
+            embed.add_field(name = 'Your cards:', value=cards, inline=True)
+            embed.set_footer(text="CAH", icon_url=icon)
+            await author.send(embed=embed)
 
-    #error if user already joined
-    else:await author.send(embed=discord.Embed(title="You've already joined to the game", color=discord.Color.red()))
+        #error if user already joined
+        else:await author.send(embed=discord.Embed(title="You've already joined to the game", color=discord.Color.red()))
 
 #list your cards
 @bot.command(aliases = ["card", "c"])
@@ -97,12 +102,13 @@ async def help(ctx):
     embed.add_field(name=">cards", value="Your current cards", inline=False)
     embed.add_field(name=">users", value="Users in game", inline=False)
     embed.add_field(name=">ping", value="Checks the ping", inline=False)
+    embed.add_field(name=">scoreboard", value="Shows the scoreboard", inline=False)
     embed.set_footer(text="CAH", icon_url=icon) 
     await ctx.author.send(embed=embed)
 
 #send message
 async def message(member):
-    embed = discord.Embed(title=black_card,color=discord.Color.blurple())
+    embed = discord.Embed(title=f"({turn}) {black_card}",color=discord.Color.blurple())
     for n,card in enumerate(member.cards):            
         embed.add_field(name=f"[{n+1}]",value=card, inline=False)
     embed.set_footer(text="CAH", icon_url=icon)
@@ -152,52 +158,72 @@ async def generate_new_card(user, answer):
 #start the game
 @bot.command(aliases = ["s"])
 async def start(ctx):
+    global turn
+    turn=1
     global userlist
     #checks if author is a joined user
     if any([x.user == ctx.author for x in userlist]):
+        while len(whites) > len(userlist) and len(blacks):
+            #draw a black card
+            global black_card
+            black_card = choice(blacks)
+            blacks.remove(black_card)
+            #send messages to all users
+            messages = await asyncio.gather(*map(message,userlist))
+            # wait for all user's vote
+            while not all(await asyncio.gather(*map(isvoted,messages))):
+                pass
+            # get the users answers
+            answers = await asyncio.gather(*map(message2,messages,userlist))
 
-        #draw a black card
-        global black_card
-        black_card = choice(blacks)
-        blacks.remove(black_card)
-        #send messages to all users
-        messages = await asyncio.gather(*map(message,userlist))
-        # wait for all user's vote
-        while not all(await asyncio.gather(*map(isvoted,messages))):
-            pass
-        # get the users answers
-        answers = await asyncio.gather(*map(message2,messages,userlist))
+            global embed
+            embed = discord.Embed(title="Choose your favorite", color=discord.Color.blurple())
+            
+            for user, answer in zip(userlist,answers):
+                user= user.name
+                embed.add_field(name = f"{user}",value=black_card.replace("____",answer))
+                embed.set_footer(text="CAH", icon_url=icon)
+            messages = await asyncio.gather(*map(message3,userlist))
 
-        global embed
-        embed = discord.Embed(title="Cards Against Humanity", color=discord.Color.blurple())
-        
-        for user, answer in zip(userlist,answers):
-            user= user.name
-            embed.add_field(name = f"{user}",value=black_card.replace("____",answer))
+            while not all(await asyncio.gather(*map(isvoted,messages))):
+                pass
+            w = await asyncio.gather(*map(get_winner,messages))
+            w = max(set(w), key=w.count)
+            userlist[w].score+=1
+            
+            embed = discord.Embed(title="The Winner", color=discord.Color.blurple())
+            embed.add_field(name = f"{userlist[w].name}",value=black_card.replace("____",answers[w]))
             embed.set_footer(text="CAH", icon_url=icon)
-        messages = await asyncio.gather(*map(message3,userlist))
 
-        while not all(await asyncio.gather(*map(isvoted,messages))):
-            pass
-        w = await asyncio.gather(*map(get_winner,messages))
-        w = max(set(w), key=w.count)
-        userlist[w].score+=1
-        
-        embed = discord.Embed(title="The Winner", color=discord.Color.blurple())
-        embed.add_field(name = f"{userlist[w].name}",value=black_card.replace("____",answers[w]))
-        embed.set_footer(text="CAH", icon_url=icon)
+            asyncio.gather(*map(send_winner,userlist))
 
-        asyncio.gather(*map(send_winner,userlist))
+            userlist = await asyncio.gather(*map(generate_new_card, userlist, answers))
+            await asyncio.sleep(1)
 
-        userlist = await asyncio.gather(*map(generate_new_card, userlist, answers))
+            if turn % 5 == 0:
+                await asyncio.gather(*map(scoreboard,userlist))
+            turn +=1
 
     else:
         await ctx.author.send(embed=discord.Embed(title="You aren't joined. Type: **>join**", color=discord.Color.red())) # send warning to join 
 
 @bot.command(aliases=['b'])
 async def scoreboard(ctx):
-    scores=[user.score for user in userlist]
-    print(dict(sorted(dict(user.user : user.score for user in userlist),key=lambda item: item[1])))
+    # make a new variable about the userlist list
+    scoreboard_list = userlist[:]
+    scoreboard_list = sorted(scoreboard_list, key=lambda user: user.score, reverse=True)
+
+    formated_user="\n".join(x.name for x in scoreboard_list)
+    scores="\n".join(str(x.score) for x in scoreboard_list)
+    # send the scoreaboard to all user
+    embed = discord.Embed(title="Cards Against Humanity", color=discord.Color.blurple())
+    embed.add_field(name='Player:', value=formated_user, inline=True)
+    embed.add_field(name='Score:', value=scores, inline=True)
+    embed.set_footer(text="CAH", icon_url=icon)
+
+    try: await ctx.author.send(embed=embed)
+    except : await ctx.user.send(embed=embed)
+
 
 TOKEN = open("./token.txt",'r').read().replace("\n","")
 bot.run(TOKEN)
